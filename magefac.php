b015c0352560dce5658a59bf71cbe98b220ea3df
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-08-29 07:32:44 +0000

    Update magefac.php

--- magefac.php
+++ magefac.php
@@ -4,6 +4,30 @@ $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 $productCollection = $objectManager->create('Magento\Reports\Model\ResourceModel\Report\Collection\Factory'); 
 $collection = $productCollection->create('Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection'); ?>
 
+-------
+
+$collection = \Magento\Framework\App\ObjectManager::getInstance()
+                ->create('Magento\Catalog\Model\ResourceModel\Product\Collection')
+                ->addCategoryFilter($category); 
+
+----------------------------------------------------
+
+$collection = \Magento\Framework\App\ObjectManager::getInstance()
+                  ->create('Magento\Catalog\Model\ResourceModel\Product\Collection');
+ 
+\Magento\Framework\App\ObjectManager::getInstance()
+ 
+->create(Magento\Catalog\Model\Product\Visibility’)
+ 
+           ->addVisibleInCatalogFilterToCollection($collection);
+
+-----------------------------------------------------
+
+<?php $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+$product = $objectManager->create('Magento\Catalog\Model\Product')->load($_item->getProduct()->getId());
+ echo $product->getThumbnail();
+echo  $product->getProductUrl(); ?>
+
 
 
 
